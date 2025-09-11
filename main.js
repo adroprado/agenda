4c588bf762f3469deb3138ae74a268b99a33115a
Alejandro Prado <adroprado@outlook.com>
2025-09-10 18:28:18 -0600

    feat(main): añade funcion leerContactos
    -Se almacena contactos en arreglo baseDeDatos. Se almacena informacion en lS setItem. Se recupera informacion del lS y se devuelve al arreglo baseDeDatos. Se programa logica de leer contactos, se itera sobre el arreglo  para añadir elementos al DOM.

--- main.js
+++ main.js
@@ -5,32 +5,69 @@ const d = document,
   $formulario = d.querySelector(".formulario"),
   $tabla = d.querySelector(".tabla"),
   $plantilla = d.querySelector(".plantilla").content,
-  $fragmento = d.createDocumentFragment(),
-  $clon = d.importNode($fragmento, true);
+  $fragmento = d.createDocumentFragment();
 
 // ===========================================
 // Variables globales de estado
 // ===========================================
-
-// --- Almacenamos contactos en nuestra base de datos ---
-// let bd = [];
+const ls = localStorage;
+let baseDeDatos = [];
 
 // ===========================================
 // Funciónes que manejan el CRUD
 // ===========================================
+// --- Lógica crear (Create) ---
 const crearContacto = () => {
-  // --- Lógica captura información de los inputs ---
-  contacto = {
+  // --- Capturando información de los inputs ---
+  const contacto = {
     id: Date.now(),
     nombre: d.querySelector(".nombre").value,
     telefono: d.querySelector(".telefono").value,
     correo: d.querySelector(".correo").value,
   };
 
-  console.log(contacto);
+  // --- Almacenamos contactos en nuestra base de datos ---
+  baseDeDatos.push(contacto);
+  console.log(baseDeDatos);
+  $formulario.reset(); // Limpia formulario para ingresar un nuevo contacto
+  location.reload(); // Actualiza la página
 };
 
-// const leerContactos = () => {};
+// --- Almacenamos base de datos en localStorage con método "setItem(key, value)"---
+const contactosSet = () => {
+  ls.setItem("contactos", JSON.stringify(baseDeDatos));
+};
+
+// --- Extracción de contactos del localStorage con método "getItem(key)"---
+
+const contactosGet = () => {
+  let contactosDelAlmacenamientoLocal = JSON.parse(ls.getItem("contactos"));
+  baseDeDatos = contactosDelAlmacenamientoLocal || [];
+  console.log(baseDeDatos);
+};
+contactosGet();
+
+// --- Lógica lectura (Read) ---
+
+const leerContactos = () => {
+  // Limpia el contenido actual de la tabla para evitar duplicados
+  $tabla.querySelector("tbody").innerHTML = "";
+
+  baseDeDatos.forEach((el) => {
+    $plantilla.querySelector(".nombre").textContent = el.nombre;
+    $plantilla.querySelector(".telefono").textContent = el.telefono;
+    $plantilla.querySelector(".correo").textContent = el.correo;
+    $plantilla.querySelector(".btn-editar").dataset.id = el.id;
+    $plantilla.querySelector(".btn-eliminar").dataset.id = el.id;
+
+    const $clon = d.importNode($plantilla, true);
+    $fragmento.appendChild($clon);
+  });
+
+  $tabla.querySelector("tbody").appendChild($fragmento);
+};
+leerContactos();
+
 // const editarContacto = () => {};
 // const eliminarContacto = () => {};
 
@@ -39,8 +76,8 @@ const crearContacto = () => {
 // ===========================================
 $formulario.addEventListener("submit", (e) => {
   e.preventDefault();
-
   crearContacto();
+  contactosSet();
 });
 
 // ===========================================
